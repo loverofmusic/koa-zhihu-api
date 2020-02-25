2cc55c38361423c3862a5f3563c7888f95f19f40
loverofmusic <1073972000@qq.com>
2020-02-25 14:17:46 +0800

    路由鉴权

--- index.js
+++ index.js
@@ -6,19 +6,28 @@ const app = new Koa();
 const router = new Router();
 const usersRouter = new Router({prefix: "/users"});
 
+const auth = async (ctx, next)=>{
+  if(ctx.url !== "/users"){
+    ctx.throw(401);
+    //unauthorized
+    //无权的; 未经授权的
+  }
+  await next();
+}
+
 router.get("/", ctx => {
   ctx.body = "这是index页面";
 });
 
-usersRouter.get("/", ctx => {
+usersRouter.get("/", auth, ctx => {
   ctx.body = "这是用户列表页面";
 });
 
-usersRouter.post("/", ctx => {
+usersRouter.post("/", auth, ctx => {
   ctx.body = "这是创建用户页面";
 });
 
-usersRouter.get("/:id", ctx => {
+usersRouter.get("/:id", auth, ctx => {
   ctx.body = `这是用户${ctx.params.id}`;
 });
 
